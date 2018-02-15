b8402f42c9b9039d52a6d556adadf2b3b763d8f9
mihirjh <mihirkjha@gmail.com>
2018-02-15 16:32:55 +0800

    Fixed error

--- mihir.py
+++ mihir.py
@@ -4,8 +4,6 @@ import datetime
 def handler(event, context):
     data = {
     'output': 'Hello Mihir' + event["pathParameters"]["name"]
-    'timestamp': datetime.datetime.utcnow().isoformat()
-    'Life': 'Welcome to the new world Neo'
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
