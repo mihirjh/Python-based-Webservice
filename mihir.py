00bcaa6d97b5e844132ce866a49e596952b66ceb
mihirjh <mihirkjha@gmail.com>
2018-02-16 19:35:44 +0800

    Chatbot code

--- mihir.py
+++ mihir.py
@@ -1,9 +1,22 @@
 import json
 import datetime
 
+    # Sentences we'll respond with if the user greeted us
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up",)
+GREETING_RESPONSES = ["'sup bro", "hey", "*nods*", "hey you get my snap?"]
+
+def check_for_greeting(sentence):
+    """If any of the words in the user's input was a greeting, return a greeting response"""
+    for word in sentence.words:
+        if word.lower() in GREETING_KEYWORDS:
+            return random.choice(GREETING_RESPONSES)
+
+
 def handler(event, context):
+    greeting = check_for_greeting(event["pathParameters"]["name"])
+
     data = {
-    'output': 'Hello Mihir' + event["pathParameters"]["name"]
+    'output': 'Hello Mihir :' + greeting
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
